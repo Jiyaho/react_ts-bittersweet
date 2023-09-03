6904617a3c5b62a720832c6b87b1e4a3986b381a
Jiyaho <masqu2n@gmail.com>
2023-09-04 02:41:24 +0900

    ADD: Login Page Layout and CSS Styles

---
+++ client/src/pages/login/Login.styles.ts
@@ -0,0 +1,51 @@
+import styled from 'styled-components';
+import { colors } from '../../styles/colors';
+
+export const Section = styled.section`
+  width: 100%;
+  margin-top: 150px;
+  form {
+    width: 270px;
+    display: flex;
+    flex-direction: column;
+    margin: auto;
+    label {
+      text-align: center;
+      font-size: 1.2em;
+    }
+    input {
+      padding: 8px;
+      height: 25px;
+      font-size: 1.1em;
+      margin-bottom: 10px;
+      border: none;
+      border-bottom: 1px solid ${colors.textDark};
+      &:focus {
+        outline: none;
+      }
+      &:focus::placeholder {
+        color: transparent;
+      }
+    }
+  }
+  div {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    button {
+      width: 270px;
+      height: 40px;
+      font-size: 1.1em;
+      margin: 5px;
+      cursor: pointer;
+      &:focus {
+        outline-color: ${colors.accent};
+      }
+    }
+    a {
+      &:focus {
+        outline-color: ${colors.accent};
+      }
+    }
+  }
+`;
--- client/src/pages/login/Login.tsx
+++ client/src/pages/login/Login.tsx
@@ -1,6 +1,36 @@
+import { Link } from 'react-router-dom';
+import Title from '../../components/atoms/Title';
 import Layout from '../../components/layouts/Layout';
+import { Section } from './Login.styles';
+import { useState } from 'react';
 
 function Login() {
-  return <Layout>Login</Layout>;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const onChangeEmail = (e: React.ChangeEvent) => {
+    const target = e.target as HTMLInputElement;
+    setEmail(target.value);
+  };
+  const onChangePassword = (e: React.ChangeEvent) => {
+    const target = e.target as HTMLInputElement;
+    setPassword(target.value);
+  };
+  return (
+    <Layout>
+      <Title title="LOGIN" />
+      <Section>
+        <form>
+          <input type="email" placeholder="Email" value={email} onChange={onChangeEmail} />
+          <input type="password" placeholder="Password" value={password} onChange={onChangePassword} />
+        </form>
+        <div>
+          <button type="submit">Login</button>
+          <Link to={'/register'}>
+            <button>Sign up</button>
+          </Link>
+        </div>
+      </Section>
+    </Layout>
+  );
 }
 export default Login;
