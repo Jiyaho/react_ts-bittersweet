6dbab2de0f54fbb54e64d37e87667a5921b3049a
Jiyaho <masqu2n@gmail.com>
2023-09-20 02:46:52 +0900

    MODIFY: Write permission only for logged-in users

--- client/src/pages/notice/Notice.tsx
+++ client/src/pages/notice/Notice.tsx
@@ -1,19 +1,31 @@
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import ScrollButton from '../../components/atoms/ScrollButton';
 import Title from '../../components/atoms/Title';
 import Layout from '../../components/layouts/Layout';
 import NoticeTable from '../../components/notice/NoticeTable';
 import { Button } from './Notice.styles';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../features/store';
 
 function Notice() {
+  const navigate = useNavigate();
+  const isAuth = useSelector((state: RootState) => state.user.isAuth);
+  const handleButton = () => {
+    // 로그인하지 않은 사용자는 로그인 페이지로 리다이렉트
+    if (!isAuth) {
+      alert('로그인이 필요합니다.');
+      navigate('/login');
+    } else {
+      navigate('/notice-write');
+    }
+  };
+
   return (
     <Layout>
       <Title title="NOTICE" />
       <ScrollButton />
       <section>
-        <Link to={'/notice-write'}>
-          <Button>📝 WRITE</Button>
-        </Link>
+        <Button onClick={handleButton}>📝 WRITE</Button>
         <article>
           <NoticeTable />
         </article>
