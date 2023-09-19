635f5e59fb234137f1ef59acfadc7c10d327061b
Jiyaho <masqu2n@gmail.com>
2023-09-20 02:28:51 +0900

    MODIFY: Fix the writer with logged-in user's name

--- client/src/pages/notice/NoticeWrite.tsx
+++ client/src/pages/notice/NoticeWrite.tsx
@@ -1,20 +1,20 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Title from '../../components/atoms/Title';
 import Layout from '../../components/layouts/Layout';
 import { useNavigate } from 'react-router-dom';
 import { Section } from './NoticeWrite.styles';
-import { newPosting } from '../../features/postingSlice';
 import { useDispatch } from 'react-redux';
-import { AppDispatch } from '../../features/store';
 import axios from 'axios';
 import { postingsUrl } from '../../utils/constants';
+import { useAuth } from '../../Api/UsersApi';
 
 function NoticeWrite() {
   const navigate = useNavigate();
   const [writer, setWriter] = useState('');
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
-  const dispatch: AppDispatch = useDispatch();
+  const dispatch = useDispatch();
+  const { data: authUser } = useAuth();
   const onChangeWriter = (e: React.ChangeEvent<HTMLInputElement>) => {
     setWriter(e.target.value);
   };
@@ -41,34 +41,22 @@ function NoticeWrite() {
       console.log('글 등록 에러: ' + error);
     }
   };
-  // const handleSubmit = async (e: React.FormEvent) => {
-  //   e.preventDefault();
-  //   const body = {
-  //     writer,
-  //     title,
-  //     content,
-  //   };
-  //   try {
-  //     await dispatch(newPosting(body));
-  //     alert('등록 완료!');
-  //     navigate('/notice');
-  //   } catch (error) {
-  //     console.log(error);
-  //     alert('Error: 글이 등록되지 않았습니다.');
-  //   }
-  // };
+
   const onClickCancle = () => {
     if (window.confirm('등록을 취소 하시겠습니까?')) {
       navigate('/notice');
     } else return;
   };
 
+  useEffect(() => {
+    setWriter(authUser.name);
+  }, []);
   return (
     <Layout>
       <Title title="WRITE" />
       <Section>
         <form onSubmit={onSubmitHandler}>
-          <input type="text" placeholder="이름을 입력해 주세요." required max="10" onChange={onChangeWriter} value={writer} />
+          <input type="text" max="10" onChange={onChangeWriter} value={authUser.name} readOnly />
           <input type="text" placeholder="제목을 입력해 주세요." required max="10" onChange={onChangeTitle} value={title} />
           <textarea cols={50} rows={10} required placeholder="내용을 입력해 주세요." onChange={onChangeContent} value={content} />
           <div>
