14ef6c5744ddcb3812c9bfa66cc19f1d74626a10
Jiyaho <masqu2n@gmail.com>
2023-09-22 02:10:43 +0900

    MODIFY: Server Port

--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
-const https = require('https');
-const httpPort = 5000;
-const httpsPort = 8080;
+const PORT = process.env.PORT || 5000;
+// const https = require('https');
+// const httpPort = 5000;
+// const httpsPort = 8080;
 const config = require('./config/key');
 const postings = require('./routes/postings');
 const users = require('./routes/users');
@@ -18,6 +19,7 @@ const corsOptions = {
   origin: [
     // 클라이언트(프론트) URL 추가
     'http://localhost:3000',
+    'https://bittersweet-korea.vercel.app',
   ],
   credentials: true,
 };
@@ -42,18 +44,22 @@ mongoose
   .then(() => console.log('MongoDB Connected...'))
   .catch((err) => console.error(err));
 
-// 배포 환경 시 적용할 내용
-if (process.env.NODE_ENV === 'production') {
-  // Encrypt Key
-  const option = {};
+// // 배포 환경 시 적용할 내용
+// if (process.env.NODE_ENV === 'production') {
+//   // Encrypt Key
+//   const option = {};
 
-  // https 서버 생성 및 실행 확인
-  https.createServer(option, app).listen(httpsPort, () => {
-    console.log('https 서버 실행 포트 :: ' + httpsPort);
-  });
-} else {
-  // 개발 환경 시 적용 내용
-  app.listen(httpPort, () => {
-    console.log('http 서버 실행 성공 포트 :: ' + httpPort);
-  });
-}
+//   // https 서버 생성 및 실행 확인
+//   https.createServer(option, app).listen(httpsPort, () => {
+//     console.log('https 서버 실행 포트 :: ' + httpsPort);
+//   });
+// } else {
+//   // 개발 환경 시 적용 내용
+//   app.listen(httpPort, () => {
+//     console.log('http 서버 실행 성공 포트 :: ' + httpPort);
+//   });
+// }
+
+app.listen(PORT, () => {
+  console.log('http 서버 실행 성공 포트 :: ' + PORT);
+});
