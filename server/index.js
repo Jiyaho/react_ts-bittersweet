6612401c3569645332ad42b1eb4f20e6f3ad4fd4
Jiyaho <masqu2n@gmail.com>
2023-09-26 12:15:22 +0900

    UPDATE: Cookies options

--- server/index.js
+++ server/index.js
@@ -16,13 +16,6 @@ const corsOptions = {
 const app = express();
 
 app.use(cors(corsOptions));
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-  res.header('Access-Control-Allow-Credentials', true);
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-  next();
-});
 
 // 'application/json' 형식의 데이터를 parse해 줌
 app.use(express.json());
